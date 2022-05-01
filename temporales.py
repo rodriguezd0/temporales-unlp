d2ee53124a27656f79dc47e34fb62d741a65f897
Daniel Rodriguez 18896/6 <rodriguezd028@gmail.com>
2022-05-01 14:07:19 -0400

    Optimizada la salida del temporizador

--- temporales.py
+++ temporales.py
@@ -26,26 +26,22 @@ class cronometro:
 
     def tiempo(self):
         if not (self.detenido or self.pausado):
-            tiempo = datetime.now() - self.tiempo_inicial
+            return datetime.now() - self.tiempo_inicial
         else:
-            tiempo = self.tiempo_detenido
-
-        return tiempo
+            return self.tiempo_detenido
 
 class temporizador:
     detenido = False
-
     def iniciar(self,horas=0,minutos=0,segundos=0):
         self.tiempo_inicial = datetime.now()
-        self.horas = int(horas)
-        self.minutos = int(minutos)
-        self.segundos = int(segundos)
+        self.horas = horas
+        self.minutos = minutos
+        self.segundos = segundos
 
     def tiempo(self):
-        if not (self.detenido):
-            horas, minutos, segundos = str(datetime.now()-self.tiempo_inicial).split(':')
-            if int(horas) == self.horas and int(minutos) == self.minutos and int(float(segundos)) == self.segundos:
-                self.detenido = True
-            tiempo = datetime.now() - self.tiempo_inicial
-
-        return tiempo, self.detenido
+        horas, minutos, segundos = str(datetime.now()-self.tiempo_inicial).split(':')
+        tiempo = datetime.now() - self.tiempo_inicial
+        if int(horas) == self.horas and int(minutos) == self.minutos and int(float(segundos)) == self.segundos:
+            return tiempo, True
+        else:
+            return tiempo, False
